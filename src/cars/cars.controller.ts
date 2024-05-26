fe1ea78cc80e9fd74be000748e3e30b65c4b777c
JorgeYael-CB <jorge.yael333@gmail.com>
2024-05-25 21:36:18 -0600

    First commit

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+
+
+export class CarsController {
+
+    constructor(
+        private readonly carsService:CarsService,
+    ){};
+
+    @Get()
+    getAllCars(){
+        return this.carsService.findAll();
+    };
+
+    @Get('/:id')
+    getCardById( @Param('id') id:string ){
+        console.log( {id} );
+
+        return this.carsService.findOneById(Number(id));
+    };
+};
